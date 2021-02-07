190447750a4f6f7c0dab553ab60f38515df4bf91
Adrián López <adsalomo@gmail.com>
2021-02-07 17:55:44 -0500

    ajuste scripts

--- ansible/put.sh
+++ ansible/put.sh
@@ -13,7 +13,12 @@ while [ $counter -lt 50 ]; do
 	mysql -u root -p123456 people -e "INSERT INTO register values ($counter, '$name', '$lastName', $age)"
 	echo "$counter, $name, $lastName, $age se importaron correctamente"
 	
-	sleep 7
+#	sleep 7
 	
 done
+
+# este archivo se copia junto con people.txt a la ruta /tmp del contenedor de mysql
+# docker cp put.sh db_mysql:/tmp
+# docker cp people.txt db_mysql:/tmp
+# create table register(id int, name varchar(100), lastName varchar(100), age int(2));
 	
